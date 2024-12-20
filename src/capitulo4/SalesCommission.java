b16d0d010a8fe395953548d44d810a10acf33327
LeandroBarreiro <leandrobarreiro15@gmail.com>
2024-12-20 11:21:49 -0400

    modified:   .vscode/launch.json 	modified:   src/capitulo4/SalesCommission.java 	deleted:    src/capitulo4/SalesCommissionTest.java

--- src/capitulo4/SalesCommission.java
+++ src/capitulo4/SalesCommission.java
@@ -1,59 +1,45 @@
 package capitulo4;
 
+import java.util.Scanner;
+
 
 public class SalesCommission {
-    private int week;
-    private double commission = 0.09;
-    private double totalSales;
-    private int item;
-    private int valueItem;
+    public static void main(String[] args) {
+        
+        Scanner input = new Scanner(System.in);
 
+        double[] valueItems = {239.99, 129.75, 99.95, 350.89};
 
-    public SalesCommission(int week, double commission, double totalSales, int item, int valueItem){
-        this.week = week;
-        this.commission = commission;
-        this. totalSales = totalSales;
-        this.item = item;
-        this.valueItem = valueItem;
-    }
+        double baseSalary = 200.00;
+        double commission = 0.09;
 
-    public int getWeek() {
-        return week;
-    }
+        System.out.println("Sales commission calculator:");
+        System.out.println("Select the item old (1-4): (Enter 0 to exit)");
 
-    public void setWeek(int week) {
-        this.week = week;
-    }
+        double totalSale = 0.0;
+        while (true) {
+            System.out.println("Item sold:");
+            int item = input.nextInt();
 
-    public double getCommission() {
-        return commission;
-    }
+            if (item == 0) {
+                System.out.println("Closing calculator");
+                break;
+            }
+            else if(item >= 1 && item <= 4) {
+                totalSale += valueItems[item -1];
+            }
+            else {
+                System.out.println("Invalid item. Enter a number between 1 and 4 or 0 to exit.");
+            }
+        }
 
-    public void setCommission(double commission) {
-        this.commission = commission;
-    }
+        double totalCommission = totalSale * commission;
+        double totalSalary = baseSalary + totalCommission;
 
-    public double getTotalSales() {
-        return totalSales;
-    }
+        System.out.printf("Total gross sales: R$%.2f.\n", totalSale);
+        System.out.printf("Commission: R$%.2f.\n", totalCommission);
+        System.out.printf("Total Salary: R$%.2f.\n", totalSalary);
 
-    public void setTotalSales(double totalSales) {
-        this.totalSales = totalSales;
-    }
-
-    public int getItem() {
-        return item;
-    }
-
-    public void setItem(int item) {
-        this.item = item;
-    }
-
-    public int getValueItem() {
-        return valueItem;
-    }
-
-    public void setValueItem(int valueItem) {
-        this.valueItem = valueItem;
+        input.close();
     }
 }
